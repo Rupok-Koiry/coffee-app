b1e4b95af8a89d00f879a75183cf4cd60e63cb99
Rupok-Koiry <koiry.rupok@gmail.com>
2025-03-28 23:55:35 +0600

    Splash Screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,33 @@
-import React from "react";
-import { Redirect } from "expo-router";
+import React, { useEffect } from "react";
+import { Image } from "react-native";
+import { useRouter } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useColorScheme } from "nativewind";
+import { Colors } from "@/constants/Colors";
 
 const Index = () => {
-  return <Redirect href="/(tabs)/product" />;
+  const { colorScheme } = useColorScheme();
+  const router = useRouter();
+  useEffect(() => {
+    const redirectTimer = setTimeout(() => {
+      router.replace("/(tabs)/product");
+    }, 50);
+    return () => clearTimeout(redirectTimer);
+  }, [router]);
+
+  return (
+    <SafeAreaView
+      style={{
+        backgroundColor: Colors[colorScheme].primaryBackgroundHex,
+        flex: 1,
+      }}
+    >
+      <Image
+        source={require("@/assets/images/splash.png")}
+        className="w-full h-full"
+      />
+    </SafeAreaView>
+  );
 };
 
 export default Index;
